e476ec43c3cd24ae4378c7370f9e3ab570777394
LeeoSilva <leeosilva0909@gmail.com>
2020-11-26 18:42:32 -0300

    adding new models and new migrations to the database

---
+++ database/migrations/2020_11_26_213427_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('nome');
+            $table->string('descricao');
+            $table->float('valor', 8, 2);
+            $table->integer('estoque'); 
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps(); // data e hora de cadastro
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
